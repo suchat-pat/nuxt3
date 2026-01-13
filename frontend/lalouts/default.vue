5d6d24fdd47a434c6118c6ce4f83376b85ab1035
suchat55210 <suchacce@gmail.com>
2026-01-13 11:39:27 +0700

    test2

---
+++ frontend/lalouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+    <v-app>
+        <v-app-bar color="#7d0c14" class="py-2">
+            <v-app-bar-nav-icon ></v-app-bar-nav-icon>
+        </v-app-bar>
+    </v-app>
+</template>
+
+<script setup lang="ts">
+const {mdAndDown} =
+</script>
+
+<style scoped>
+
+</style>
---
+++ frontend/plugins/vuetify.ts
@@ -0,0 +1,7 @@
+import 'vuetify/styles'
+import '@mdi/font/css/materialdesignicons.css'
+import {createVuetify} from 'vuetify'
+export default defineNuxtPlugin((app) =>{
+    const vuetify = createVuetify({})
+    app.vueApp.use(vuetify)
+})
