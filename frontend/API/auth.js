53bb5eca86f331b58625dfd78623b59ff0838c2a
peet5228 <ppeet5228@gmail.com>
2026-01-13 12:55:28 +0700

    add

--- frontend/API/auth.js
+++ frontend/API/auth.js
@@ -1,3 +1,3 @@
 import axios from 'axios'
-const api = 'http://localhost:3001/auth/login'
+const api = 'http://localhost:3001/api/auth/login'
 export default {login : (data) => axios.post(`${api}`,data)}
